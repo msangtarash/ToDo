ea2c1caa83c2ea1597e93739098e25c16abbbdbb
ysmoradi <ysmoradi@outlook.com>
2017-10-21 18:30:34 +0330

    Added ios & upw projects + xamarin forms

---
+++ src/ToDo.iOS/AppDelegate.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Foundation;
+using Prism.Autofac.Forms;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace ToDo.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            LoadApplication(new App(new iOSInitializer()));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class iOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainer container)
+        {
+
+        }
+    }
+
+}
