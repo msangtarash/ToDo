6acb133150e23898620072219981073459701685
ysmoradi <ysmoradi@outlook.com>
2017-11-11 21:31:07 +0330

    Removed shadow of icon button in android

---
+++ src/ToDo.Droid/Renderes/ToDoIconButtonRenderer.cs
@@ -0,0 +1,24 @@
+﻿using FormsPlugin.Iconize;
+using FormsPlugin.Iconize.Droid;
+using ToDo.Views;
+using ToDo.Droid.Renderes;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ToDoIconButton), typeof(ToDoIconButtonRenderer))]
+
+namespace ToDo.Droid.Renderes
+{
+    public class ToDoIconButtonRenderer : IconButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.StateListAnimator = null;
+            }
+        }
+    }
+}
